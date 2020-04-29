094faea5c28d87de43c70f05d8e86ee1601d6dc3
amirali <amirali.mnj@gmail.com>
2020-04-29 08:02:53 +0430

    This is my initial commit.

---
+++ app/src/main/java/com/example/androidworkshoplistview/Person.java
@@ -0,0 +1,35 @@
+package com.example.androidworkshoplistview;
+
+public class Person {
+    private String name, birthday, sex;
+
+    public Person(String name, String birthday, String sex) {
+        this.name = name;
+        this.birthday = birthday;
+        this.sex = sex;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
